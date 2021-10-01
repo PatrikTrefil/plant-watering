88f80f4d58b5860291cfc67116516f982ff11e42
Patrik Trefil <patrik.trefil@gmail.com>
2021-10-01 19:07:27 +0200

    don't write to error_log, print to stderr instead

--- plant_watering/error.py
+++ plant_watering/error.py
@@ -8,11 +8,5 @@ class ErrorLog:
   led = Led(20)
 
   @staticmethod
-  def log_error(text):
-    config = get_config()
-    with open(config["error_log"], "w") as error_file:
-      error_file.write(text)
-
-  @staticmethod
   def signal_error():
     ErrorLog.led.flash(10)
