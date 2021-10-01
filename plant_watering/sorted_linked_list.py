ecbad1d280de90accc13affab8ff4159bfa5b52b
Patrik Trefil <patrik.trefil@gmail.com>
2021-10-01 13:14:52 +0200

    fix sorted linked list variable name

--- plant_watering/sorted_linked_list.py
+++ plant_watering/sorted_linked_list.py
@@ -18,18 +18,18 @@ class SortedLinkedList:
       curr_node = self.start_node
       while new_node.value > curr_node.value:
         prev_node = curr_node
-        curr_node = curr_node.next
+        curr_node = curr_node.next_item
       # add new node between prev and curr
       if prev_node is None:
         self.start_node = new_node
-        new_node.next = curr_node
+        new_node.next_item = curr_node
       else:
-        prev_node.next = new_node
-        new_node.next = curr_node
+        prev_node.next_item = new_node
+        new_node.next_item = curr_node
 
   def add_item(self, item):
     self.add_node(Node(item))
 
   def remove_start_node(self):
     if self.start_node is not None:
-      self.start_node = self.start_node.next
+      self.start_node = self.start_node.next_item
