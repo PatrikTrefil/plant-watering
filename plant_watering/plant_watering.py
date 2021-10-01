9f371852c83db35206db0892fdae02771e07b6b3
Patrik Trefil <patrik.trefil@gmail.com>
2021-10-01 09:38:23 +0200

    logging to repo

--- plant_watering/plant_watering.py
+++ plant_watering/plant_watering.py
@@ -8,6 +8,7 @@ from scheduler import Scheduler
 import RPi.GPIO as GPIO
 import plant
 import events
+import git_log
 from config import get_config
 
 
@@ -32,9 +33,11 @@ def main():
     if sender.last_res < sender.min_humidity:
       scheduler.add_event(events.LackOfWater(datetime.datetime.now(), sender))
   events.MeasurementDone.add_event_listener(water_if_needed)
+  events.MeasurementDone.add_event_listener(lambda sender: git_log.log_to_repo(sender.last_res))
 
   # water when needed
   events.LackOfWater.add_event_listener(lambda sender: sender.water())
+  events.LackOfWater.add_event_listener(lambda sender: git_log.log_to_repo(f"Watered {sender.name} at {datetime.datetime.now()}"))
   # day scheduling
   def schedule_day(sender):
     for plant_item in plant_list:
