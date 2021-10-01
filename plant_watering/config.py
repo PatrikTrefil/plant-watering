24b44b19113d27aec43c9f0c8664482bc9b318c2
Patrik Trefil <patrik.trefil@gmail.com>
2021-10-01 18:48:48 +0200

    comments

--- plant_watering/config.py
+++ plant_watering/config.py
@@ -4,6 +4,7 @@
 import json
 
 def get_config():
+  """returns config object"""
   CONFIG_FILE_PATH = "./config.json"
   with open(CONFIG_FILE_PATH, "r") as config_file:
     return json.load(config_file)
