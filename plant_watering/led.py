cb9281273e5f44c83403514394a08a4bf3b69b70
Patrik Trefil <patrik.trefil@gmail.com>
2021-10-07 18:09:19 +0200

    internal note of state in led

--- plant_watering/led.py
+++ plant_watering/led.py
@@ -5,10 +5,12 @@ import time
 import RPi.GPIO as GPIO
 
 class Led:
+  is_on = False
   def __init__(self, pin_num):
     self.pin_num = pin_num
 
   def turn_on_off(self, is_on):
+    self.is_on = is_on
     GPIO.output(
       self.pin_num,
       GPIO.HIGH if is_on is True else GPIO.LOW
