8614dcef4f0e664dbfc0e8bc8e4ffe3f6c2c905c
Patrik Trefil <patrik.trefil@gmail.com>
2021-10-07 18:48:31 +0200

    default to GPIO.HIGH in relay

--- plant_watering/relay.py
+++ plant_watering/relay.py
@@ -5,7 +5,7 @@ import RPi.GPIO as GPIO
 class Relay:
   def __init__(self, pin_num):
     self.pin_num = pin_num
-    GPIO.setup(pin_num, GPIO.OUT, initial=GPIO.LOW) # default to 0 V
+    GPIO.setup(pin_num, GPIO.OUT, initial=GPIO.HIGH)
 
   def switch(self, on):
     GPIO.output(
